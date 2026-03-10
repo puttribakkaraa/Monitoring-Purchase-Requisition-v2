e2389b0a4b0a7f4f13611f4fc46ee170dccd45c5
Putri Valentina Bakkara <putrivalentinabakkara@gmail.com>
2026-03-10 09:23:19 +0700

    Pembaharuan Dashboard khusu Monitoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,7 +32,6 @@ class DatabaseSeeder extends Seeder
                 'role' => 'monitoring'
             ]
         );
-
         $this->call([
             PurchaseRequisitionSeeder::class,
         ]);
